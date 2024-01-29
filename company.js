e55b69dac519880f5f63658e7a953c83ee93ace1
Mr Hanzla <mr.hanzla101@gmail.com>
2024-01-29 11:46:16 +0500

    call methods and constants from separate module

--- company.js
+++ company.js
@@ -12,30 +12,9 @@ const dbConfig = {
 
 const pool = new Pool(dbConfig);
 
-const Departments = Object.freeze({
-    Admin: 1,
-    Marketing: 2,
-    Sales: 3,
-    Production: 4,
-    Legal: 5
-});
-
 // ==========================================================
-function getAllCompanyData() {
-    const allCompanyQuery = 'SELECT * FROM company;';
-    return pool.query(allCompanyQuery); 
-}
-
-function getCompanyDataByName(name) {
-    const allCompanyQuery = 'SELECT * FROM company WHERE company_name = $1;';
-    return pool.query(allCompanyQuery, [name]); 
-}
-
-function respondErrorMessage(res, error) {
-    console.error('Error adding data to the database:', error);
-    res.status(500).json({ err: 'Internal Server Error ' + error });
-    console.log(error);
-}
+const commons = require('./commons.js');
+const constants = require('./constants.js');
 // ==========================================================
 
 router.get('/', async (req, res) => {
@@ -44,8 +23,8 @@ router.get('/', async (req, res) => {
         const { companyName } = req.body;
 
         // If company name is present, search with company name
-        const { rows } = companyName ? await getCompanyDataByName(companyName)
-        : await getAllCompanyData();
+        const { rows } = companyName ? await commons.getCompanyDataByName(companyName)
+        : await commons.getAllCompanyData();
 
         // Send the query result as JSON response
         res.status(201).json(rows);
@@ -64,20 +43,20 @@ router.post('/register', async (req, res) => {
     const companyInsertQuery = 'INSERT INTO company (company_name, company_description) VALUES ($1, $2) RETURNING company_id;';
     pool.query(companyInsertQuery, [companyName, companyDescription], (err, result) => {
         if (err) {
-            respondErrorMessage(res, err);
+            commons.respondErrorMessage(res, err);
         } else {
             const companyId = result.rows[0].company_id;
             // step-2: once company is registered, add a department to it
             const depInsertQuery = 'INSERT INTO company_department (company_id, department_id) VALUES ($1, $2);';
-            pool.query(depInsertQuery, [companyId, Departments.Admin], (depError, depResults) => {
+            pool.query(depInsertQuery, [companyId, constants.Departments.Admin], (depError, depResults) => {
                 if (depError) {
-                    respondErrorMessage(res, depError);
+                    commons.respondErrorMessage(res, depError);
                 } else {
                     // step-3: once company is registered with a department, add a employee
                     const employeeInsertQuery = 'INSERT INTO employee (employee_name, employee_email, employee_password, department_id, company_id) VALUES ($1, $2, $3, $4, $5);';
-                    pool.query(employeeInsertQuery, [employeeName, email, password, Departments.Admin, companyId], (empError, empResults) => {
+                    pool.query(employeeInsertQuery, [employeeName, email, password, constants.Departments.Admin, companyId], (empError, empResults) => {
                         if (empError) {
-                            respondErrorMessage(res, empError);
+                            commons.respondErrorMessage(res, empError);
                         } else {
                             // Send a success response
                             res.status(201).json({ message: `'${companyName}' is registered!` });
