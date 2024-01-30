36d4eddde39677d31e9c2d6e153f9e810abb5530
Mr Hanzla <mr.hanzla101@gmail.com>
2024-01-30 09:26:04 +0500

    add functions to insert and delete company data

--- commons.js
+++ commons.js
@@ -1,18 +1,26 @@
 const { Pool } = require('pg');
 
-const dbConfig = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'pf',
-    password: 'asdf1234',
-    port: 5432,
-};
-
+const dbConfig = require('./connection.json');
 const pool = new Pool(dbConfig);
+
+async function getCompanyId(companyName) {
+    const allCompanyQuery = 'SELECT company_id FROM company WHERE company_name = $1;';
+    try {
+        const results = await pool.query(allCompanyQuery, [companyName]);
+        if (results.rows.length > 0) {
+            return results.rows[0].company_id;
+        } else {
+            // Company not found
+            return -1;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
 
 function getAllCompanyData() {
     const allCompanyQuery = 'SELECT * FROM company;';
-    return pool.query(allCompanyQuery); 
+    return pool.query(allCompanyQuery);
 }
 
 function getCompanyDataByName(name) {
@@ -20,14 +28,20 @@ function getCompanyDataByName(name) {
     return pool.query(allCompanyQuery, [name]); 
 }
 
+async function deleteCompanyById(companyId) {
+    const deleteCompanyQuery = 'DELETE FROM company WHERE company_id = $1;';
+    return await pool.query(deleteCompanyQuery, [companyId]); 
+}
+
 function respondErrorMessage(res, error) {
     console.error('Error adding data to the database:', error);
-    res.status(500).json({ err: 'Internal Server Error ' + error });
-    console.log(error);
+    res.status(500).json({ err: 'Internal Server Error: ' + error.hint ? error.hint : error });
 }
 
 module.exports = {
     getAllCompanyData,
     getCompanyDataByName,
-    respondErrorMessage
+    respondErrorMessage,
+    deleteCompanyById,
+    getCompanyId
 };
